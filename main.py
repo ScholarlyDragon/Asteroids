a326ad4c732da1d11b06ccfecb69bce761ce0c4e
sDragon <sethjb36@gmail.com>
2025-11-23 19:30:56 -0500

    4. add clock with 60 fps refresh rate to game loop

--- main.py
+++ main.py
@@ -10,6 +10,11 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     
+    #creates a new clock object
+    clock = pygame.time.Clock()
+    dt = 0
+
+    #game loop
     while True:
         log_state()
         for event in pygame.event.get():
@@ -17,6 +22,8 @@ def main():
                 return
         screen.fill("black")
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
+
 
 if __name__ == "__main__":
     main()
