54ce65c2d02b5c83936e7baed8fd79aa77e15a52
sDragon <sethjb36@gmail.com>
2025-11-26 20:44:39 -0500

    10. Created asteroid and asteroidfield and implemented into game logic

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import pygame
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from logger import log_state
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
@@ -10,23 +12,31 @@ def main():
 
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    
-    #creates a new clock object
-    clock = pygame.time.Clock()
-    dt = 0
 
     #groups
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
     Player.containers = (updatable, drawable)
-        
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
+
     #creates a new player object centered on screen
     x = SCREEN_WIDTH / 2 
     y = SCREEN_HEIGHT / 2
     player1 = Player(x, y)
 
+    #creates a new clock object
+    clock = pygame.time.Clock()
+    dt = 0
+
+    #creates a new AsteroidField object
+    asteroidfield1 = AsteroidField()
+
     # infinite game loop
-    while True:
+    running = True
+    while running:
         log_state()
         # make the "X" button work
         for event in pygame.event.get():
@@ -36,8 +46,8 @@ def main():
         updatable.update(dt)
         # draw everything
         screen.fill("black")
-        for item in drawable:
-            item.draw(screen)
+        for sprite in drawable:
+            sprite.draw(screen)
         # limit frame rate
         dt = clock.tick(60) / 1000
         # show the frame
